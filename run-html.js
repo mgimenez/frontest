65a93db21e85bc815f30b568e1c2db7ee012e61c
mgimenez <matias.gimenez@gmail.com>
2015-10-08 19:04:44 -0300

    add ace editor

--- run-html.js
+++ run-html.js
@@ -3,29 +3,39 @@
 
 	window.addEventListener('load', function(evt) {
 
-		var form = doc.querySelector('form'),
-			data = {};
+		var editor = initAce();
 
+		var form = doc.querySelector('form'),
+			btnMore = doc.querySelectorAll('.btn-more'),
+			data = {};
 		form.addEventListener('submit', function(e) {
 			e.preventDefault();
 			// var cssResources = '<link rel="stylesheet" href="' + doc.querySelector('#css-resource').value + '"/>';
 			// var jsResources = '<script src="' + doc.querySelector('#js-resource').value + '"</script>';
 			// var loc = 'data:text/html, ' + cssResources + doc.querySelector('.html-code').value + jsResources;
-			var loc = 'data:text/html, ' + doc.querySelector('.html-code').value;
+			var loc = 'data:text/html, ' + editor.getValue();
 			chrome.tabs.create( { url: loc}, function() {chrome.devtools.inspectedWindow});
 			
 		});
 
-		form.addEventListener('focus', function(e) {
-			var currentFocus = e.target,
-				name = currentFocus.name;
-
-			form.addEventListener('keypress', function(e) {
-				data[name] = currentFocus.value;
-
+		for (var i = 0; i < btnMore.length; i++) {
+			btnMore[i].addEventListener('click', function(e) {
+				e.preventDefault();
 			});
+		}
 
-		});
+		// form.addEventListener('focus', function(e) {
+		// 	var currentFocus = e.target,
+		// 		name = currentFocus.name;
+
+		// 	form.addEventListener('keypress', function(e) {
+		// 		data[name] = currentFocus.value;
+
+		// 	});
+
+		// });
+
+		
 	});
 
 
@@ -37,6 +47,14 @@
     chrome.storage.sync.get('value', function(data) {
     	console.log(data.value);
     })
+
+    function initAce() {
+		var editor = ace.edit("editor");
+		    editor.setTheme("ace/theme/monokai");
+		    editor.getSession().setMode("ace/mode/html");
+		    editor.getSession().setUseWorker(false);
+		return editor;
+    }
 
 
 
