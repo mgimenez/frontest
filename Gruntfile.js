e6109af5ba1ae2fc1ab44c27b08c97e49bd05359
mgimenez <matias.gimenez@gmail.com>
2015-11-26 17:44:14 -0300

    updating gruntfile with new js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,16 @@ module.exports = function(grunt) {
                     },{
                         expand: true,
                         cwd: 'src/js',
-                        src: ['ace.js', 'mode-html.js', 'theme-monokai.js'],
+                        src: ['ace.js',
+                            'ext-language_tools.js',
+                            'mode-css.js',
+                            'mode-javascript.js',
+                            'mode-html.js', 
+                            'worker-css.js',
+                            'worker-javascript.js',
+                            'worker-html.js', 
+                            'theme-monokai.js'
+                        ],
                         dest: 'build/js'
                     }
                 ]
